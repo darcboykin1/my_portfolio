9e5b0c115e94838df6d3823a916406aca39ee4d6
Darc Boykin <darcboykin@gmail.com>
2017-02-21 10:13:55 -0500

    m

--- portfolio/src/Head.js
+++ portfolio/src/Head.js
@@ -6,7 +6,7 @@ class Head extends Component {
   render() {
     return (
       <div className="uk-container uk-container-center containment">
-      <img src="http://placehold.it/200x200" alt="self" id="me" className="uk-thumbnail uk-overlay-hover uk-border-circle"/>
+      <img src="http://placehold.it/200x200" alt="self" id="me" className="uk-thumbnail uk-overlay-hover uk-border-circle uk-position-center"/>
       </div>
     );
   }
